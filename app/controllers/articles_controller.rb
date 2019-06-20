d895c0b9bf07ca2f857bdf4016be2f4e77c8f0c3
13events <deadworlds@me.com>
2019-06-19 21:31:13 -0700

    Create Users table and model, fix bundler conflics

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,68 +1,62 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
+  # Call the 'set_article' method before performing the follwing actions
+  before_action :set_article, only: %i[edit update show destroy]
 
-	#Call the 'set_article' method before performing the follwing actions
-	before_action :set_article, only: [:edit, :update, :show, :destroy]
+  def index
+    # grab all articles from database
+    @articles = Article.all
+  end
 
-	def index
-		#grab all articles from database
-		@articles = Article.all
-	end
-	def new
-		@article = Article.new
-	end
+  def new
+    @article = Article.new
+  end
 
-	def edit
-		
-	end
+  def edit; end
 
-	def update
+  def update
+    if @article.update(article_params)
+      flash[:success] = 'Article Successfully Updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
 
-		if @article.update(article_params)
-			flash[:success] = "Article Successfully Updated"
-			redirect_to article_path(@article)
-		else
-			render 'edit'
-		end
+  def create
+    # render plain: params[:article].inspect
+    @article = Article.new(article_params)
 
-	end
+    # check and handle failed article save
+    if @article.save
+      flash[:success] = 'Article Successfully Created'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
 
-	def create
-		#render plain: params[:article].inspect
-		@article = Article.new(article_params)
-		
-		#check and handle failed article save
-		if @article.save
-			flash[:success] = "Article Successfully Created"
-			redirect_to article_path(@article)
-		else 
-			render 'new'
-		end
-	end
+  def show; end
 
-	def show
-		
-	end
+  def destroy
+    # destroy the article
+    @article.destroy
 
-	def destroy
+    # flash message
+    flash[:danger] = 'Article Successfully deleted.'
 
-		 #destroy the article
-		 @article.destroy
+    # redirect to articles index page
+    redirect_to articles_path
+  end
 
-		 #flash message
-		 flash[:danger] = "Article Successfully deleted."
+  private
 
-		 #redirect to articles index page
-		 redirect_to articles_path
-	end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-	private 
-
-	def set_article
-		@article = Article.find(params[:id])
-	end
-
-	def article_params
-		params.require(:article).permit(:title, :description)
-	end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
- 
