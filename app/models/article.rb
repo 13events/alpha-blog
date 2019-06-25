efd799a993c59a2e125fa509bc919b040e4286c0
13events <deadworlds@me.com>
2019-06-25 14:11:35 -0700

    Fixed some styling issues.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
+# Represents an Article object with validates title, description and user_id
+# Article object has an association to user.
 class Article < ApplicationRecord
-
   belongs_to :user
 
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 10_000 }
-  validates :user_id, presence: true
+  validates :title,
+            presence: true,
+            length: { minimum: 3, maximum: 50 }
+  validates :description,
+            presence: true,
+            length: { minimum: 10, maximum: 10_000 }
+  validates :user_id,
+            presence: true
 end
