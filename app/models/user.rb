b097ff546e59730883fd3f63cf8de35c1fede867
13events <deadworlds@me.com>
2019-06-24 19:10:44 -0700

    Add secure password and attribute to users.
    Added bcrypt gem to support passwords and authentication.
    Added attribute 'has_secure_password' to user.
    added password_digest column to User table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
 
+  has_secure_password
 	has_many :articles
-	 before_save { self.email = email.downcase }
+	before_save { self.email = email.downcase }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	validates :username, presence: true,
